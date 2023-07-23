a35fc39a02b186d4ca01676ee03cff06515febe6
Nathan Turkson <nathanturkson@gmail.com>
2023-07-23 23:06:18 +0000

    Main App edits.. added themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import CardGroup from "./components/CardGroup";
+import { useState } from "react";
+import developerData from "./data.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (searchValue) => {
+    setSearchTerm(searchValue);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header onSearch={handleSearch} />
+      <CardGroup devData={developerData} searchTerm={searchTerm} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
